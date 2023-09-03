cca93aeac79e973bef136d9b8f57b691545af7ff
KokiHirokawa <kitakitsune1110@gmail.com>
2023-09-03 23:35:08 +0900

    Initial Commit

---
+++ 10-days-of-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  10-days-of-swiftui
+//
+//  Created by Koki Hirokawa on 2023/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 10-days-of-swiftui/_0_days_of_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_days_of_swiftuiApp.swift
+//  10-days-of-swiftui
+//
+//  Created by Koki Hirokawa on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct _0_days_of_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
