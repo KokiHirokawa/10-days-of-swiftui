d00ec3e33c68d4ea617ad10c0f32802787faf175
KokiHirokawa <kitakitsune1110@gmail.com>
2023-09-04 13:05:13 +0900

    Day 02

---
+++ day-02.swiftpm/ContentView.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showsBorder = false
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 32) {
+            Button {
+                print("Tapped")
+            } label: {
+                buttonLabel
+            }
+
+            Toggle(isOn: $showsBorder) {
+                Text("Shows border")
+            }
+            .frame(width: 200)
+        }
+    }
+
+    @ViewBuilder
+    private var buttonLabel: some View {
+        let capsule = Capsule()
+            .fill(.orange)
+            .frame(width: 200, height: 48)
+            .padding(.horizontal, 20)
+            .padding(.vertical, 12)
+            .overlay {
+                Text("Button")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundStyle(.white)
+            }
+
+        if showsBorder {
+            capsule
+                .background(alignment: .center) {
+                    Rectangle()
+                        .stroke(.black, lineWidth: 1.0)
+                }
+        } else {
+            capsule
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
