6c82c5e9cb1f5016615c81ed3a35b539f7518ca5
KokiHirokawa <kitakitsune1110@gmail.com>
2023-09-04 14:01:58 +0900

    Day 02 - add comment

--- day-02.swiftpm/ContentView.swift
+++ day-02.swiftpm/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
             }
 
             Toggle(isOn: $showsBorder) {
-                Text("Shows border")
+                Text("Border")
             }
             .frame(width: 200)
         }
@@ -24,7 +24,7 @@ struct ContentView: View {
             .fill(.orange)
             .frame(width: 200, height: 48)
             .padding(.horizontal, 20)
-            .padding(.vertical, 12)
+            .padding(.vertical, 12) // padding はタップ領域に含まれる
             .overlay {
                 Text("Button")
                     .font(.system(size: 20, weight: .bold))
